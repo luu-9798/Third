9e84e3c30898f703428f810b9267bffed49e3bc8
tluu9798 <trungluu9798@gmail.com>
2023-01-05 15:47:48 +0700

    Create Third Swift app

---
+++ Shared/ThirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdApp.swift
+//  Shared
+//
+//  Created by Trung Luu on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ThirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
